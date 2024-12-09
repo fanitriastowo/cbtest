85efec75db71df2220d1cd9aaa82214e0dc07588
Fani Triastowo <fani.triastowo@gmail.com>
2024-12-09 11:47:46 +0700

    infra: added env example

---
+++ config/.env.example.exs
@@ -0,0 +1,4 @@
+System.put_env("POSTGRES_USERNAME", "postgres")
+System.put_env("POSTGRES_PASSWORD", "postgres")
+System.put_env("POSTGRES_HOST", "localhost")
+System.put_env("POSTGRES_DB", "cbtest_dev")
